12036b98c03c4d94ed23854ef4715f3a7a5c7b00
Luiz Barletta <luiz@bildzinkas.uk>
2022-07-08 23:22:31 +0100

    Adding dependencies, adding the web server to main, adding configuration file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use actix_web::{App, HttpServer};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Config {
+    connection: String,
+}
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            connection: Default::default(),
+        }
+    }
+}
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    let cfg: Config = confy::load("rust-todo-api").unwrap();
+
+    dbg!(cfg);
+    HttpServer::new(|| App::new())
+        .bind("0.0.0.0:9090")?
+        .run()
+        .await
 }
